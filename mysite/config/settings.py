ed9b8d83d358006333a163e375f3eeee504e4366
DongYeol Lee <dlehdduf0902@naver.com>
2022-06-27 03:33:33 +0900

    2-02 모델 수행 1. 장고 앱 migrate 해당 앱들이 필요로 하는 데이터베이스 테이블 생성.  > migrate를 수행하면 admin, auth, contenttypes, sessions 앱들이 사용하는 테이블들이 생성된다. 2. 모델 작성 -파이보는 질문과 답변을 하는 게시판 서비스 -질문(Question)과 답변(Answer)에 해당되는 모델을 pybo/models.py 파일에 정의 3. 작성한 모델을 이용해 테이블 생성 -테이블 생성을 위해 가장 먼저 해야 할 일은 pybo 앱을 config/settings.py 파일의 INSTALLED_APPS 항목에 추가하는 일이다.
    -makemigration 명령 수행
    > 모델이 신규로 생성 or 변경되면 migrate명령 전에 수행해야 됨.
    >pybo\migrations\0001_initial.py 라는 파이썬 파일이 자동으로 생성된다.
    4.migrate
    -실제 테이블 생성
    5. 장고 셸로 모델 실행

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pybo.apps.PyboConfig',#pybo/apps.py에 위치한 클래스. pybo 앱 생성시 자동으로 만들어짐
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -3,14 +3,19 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
-
+# 질문 모델에 필요한 속성들. 질문 제목, 내용, 작성한 일시
 class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.subject # id 값 대신 제목 표시
 
+# 질문, 답변의 내용, 답변 작성 일시
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)# 답변과 연결된 질문이 삭제될 경우 답변도 함께 삭제된다.
     content = models.TextField()
     create_date = models.DateTimeField()
+# 기존 모델을 속성으로 연결하려면 Foreignkey를 사용해야 한다. Foreignkey는 다른 모델과 연결하기 위해
+#사용한다.
